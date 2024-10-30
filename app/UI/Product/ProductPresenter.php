5d82fd33dd72732206d26778c6b090d66c5e4a0c
Radek Jeník <radek.jenik36@gmail.com>
2024-10-30 14:21:28 +0100

    init

---
+++ app/UI/Product/ProductPresenter.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\UI\ProductPresenter;
+
+use Nette;
+
+final class ProductPresenter extends Nette\Application\UI\Presenter
+{
+	public function __construct(
+		private Nette\Database\Explorer $database,
+	) {
+
+	}
+
+    public function actionDefault()
+    {
+        $rows = $database->fetchAll('SELECT * FROM products');
+
+        $this->sendJson($rows);
+    }
+}
---
+++ temp/cache/nette.search/1836801a4ef312b2ca020bc045fcac86.php
@@ -0,0 +1,42 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Bootstrap' => 
+    array (
+      0 => '/srv/app/Bootstrap.php',
+      1 => 1729730866,
+    ),
+    'App\\Core\\RouterFactory' => 
+    array (
+      0 => '/srv/app/Core/RouterFactory.php',
+      1 => 1729730866,
+    ),
+    'App\\UI\\Accessory\\LatteExtension' => 
+    array (
+      0 => '/srv/app/UI/Accessory/LatteExtension.php',
+      1 => 1729730866,
+    ),
+    'App\\UI\\Error\\Error4xx\\Error4xxPresenter' => 
+    array (
+      0 => '/srv/app/UI/Error/Error4xx/Error4xxPresenter.php',
+      1 => 1729730866,
+    ),
+    'App\\UI\\Error\\Error5xx\\Error5xxPresenter' => 
+    array (
+      0 => '/srv/app/UI/Error/Error5xx/Error5xxPresenter.php',
+      1 => 1729730866,
+    ),
+    'App\\UI\\Home\\HomePresenter' => 
+    array (
+      0 => '/srv/app/UI/Home/HomePresenter.php',
+      1 => 1729730866,
+    ),
+  ),
+  1 => 
+  array (
+  ),
+  2 => 
+  array (
+  ),
+);
